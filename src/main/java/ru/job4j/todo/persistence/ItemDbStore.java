3f1bebc990b8f779446802ef47d0d21829dfee48
Stanislav <yustas@protonmail.com>
2022-06-13 15:37:40 +0500

    3.1.3. Лямбды и шаблон wrapper.

--- src/main/java/ru/job4j/todo/persistence/ItemDbStore.java
+++ src/main/java/ru/job4j/todo/persistence/ItemDbStore.java
@@ -2,12 +2,14 @@ package ru.job4j.todo.persistence;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.Item;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * В классе происходит обработка заданий в базе данных.
@@ -23,36 +25,45 @@ public class ItemDbStore {
         this.sf = sf;
     }
 
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+    }
+
     /**
      * Добавление задания в базу данных.
      *
      * @param item Новое задание.
      */
     public void create(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
+        this.tx(session -> session.save(item));
     }
 
     /**
      * Обновление параметров задания по id.
      *
-     * @param item
+     * @param item Обновлённое задание.
      */
     public void update(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.createQuery("update Item set name = :newName, description = :newDescription, "
-                        + "done = :newDone where id = :id")
-                .setParameter("id", item.getId())
-                .setParameter("newName", item.getName())
-                .setParameter("newDescription", item.getDescription())
-                .setParameter("newDone", item.isDone())
-                .executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+        this.tx(
+                session -> session.createQuery("update Item set name = :newName, description = :newDescription, "
+                                + "done = :newDone where id = :id")
+                        .setParameter("id", item.getId())
+                        .setParameter("newName", item.getName())
+                        .setParameter("newDescription", item.getDescription())
+                        .setParameter("newDone", item.isDone())
+                        .executeUpdate()
+        );
     }
 
     /**
@@ -61,12 +72,10 @@ public class ItemDbStore {
      * @param id Id задания.
      */
     public void delete(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.createQuery("delete from Item where id = :id")
-                .setParameter("id", id).executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+        this.tx(
+                session -> session.createQuery("delete from Item where id = :id")
+                        .setParameter("id", id).executeUpdate()
+        );
     }
 
     /**
@@ -75,12 +84,10 @@ public class ItemDbStore {
      * @param id Id выполненного задания.
      */
     public void complete(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.createQuery("update Item set done = true where id = :id")
-                .setParameter("id", id).executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+        this.tx(
+                session -> session.createQuery("update Item set done = true where id = :id")
+                        .setParameter("id", id).executeUpdate()
+        );
     }
 
     /**
@@ -89,12 +96,9 @@ public class ItemDbStore {
      * @return Список найденных заданий.
      */
     public List findAll() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        var result = session.createQuery("from Item ").list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(
+                session -> session.createQuery("from Item ").list()
+        );
     }
 
     /**
@@ -104,13 +108,10 @@ public class ItemDbStore {
      * @return Найденный кандидат.
      */
     public Item findById(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Item result = (Item) session.createQuery("from Item where id = :id")
-                .setParameter("id", id).uniqueResult();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(
+                session -> (Item) session.createQuery("from Item where id = :id")
+                        .setParameter("id", id).uniqueResult()
+        );
     }
 
     /**
@@ -119,12 +120,9 @@ public class ItemDbStore {
      * @return Список выполненных заданий.
      */
     public List findByCompletedItems() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        var result = session.createQuery("from Item where done = true").list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(
+                session -> session.createQuery("from Item where done = true").list()
+        );
     }
 
     /**
@@ -135,13 +133,10 @@ public class ItemDbStore {
     public List findByActualItems() {
         LocalDateTime dateTime = LocalDateTime.now().minusDays(1);
         Timestamp actualTime = Timestamp.valueOf(dateTime);
-        Session session = sf.openSession();
-        session.beginTransaction();
-        var result = session
-                .createQuery("from Item where created > :actualTime ")
-                .setParameter("actualTime", actualTime).list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(
+                session -> session
+                        .createQuery("from Item where created > :actualTime ")
+                        .setParameter("actualTime", actualTime).list()
+        );
     }
 }
